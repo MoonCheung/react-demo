73276fa993dda67084d8eec8895625b1a4d5d419
MoonCheung <Salvador23@163.com>
2019-08-06 16:59:41 +0800

    update config-overrides

--- config-overrides.js
+++ config-overrides.js
@@ -34,14 +34,3 @@ module.exports = override(
     return config;
   }
 );
-
-// module.exports = function override(config, env) {
-//   //do stuff with the webpack config...
-//   //css模块化
-//   config = rewireCssModules(config, env);
-//   //配置别名
-//   config.resolve.alias = {
-//     '@': resolve('src')
-//   };
-//   return config;
-// };
